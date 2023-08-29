b76e9eabe3764f7a2c6e127c485a517a724bd806
Sophie Watson <semwatson09@gmail.com>
2023-08-29 18:02:15 +0200

    Search bar basic functionality

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,5 +1,8 @@
 class PagesController < ApplicationController
   def home
     @spaceships = Spaceship.all
+    if params[:query].present?
+      @spaceships = @spaceships.where(seats: params[:query].to_i)
+    end
   end
 end
