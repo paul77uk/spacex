143626b742bec47c6da31d8ea588250d2195e2e1
paulvickers <paul77uk@gmail.com>
2023-08-29 03:05:32 +0100

    dashboard branch

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,4 +1,9 @@
 class PagesController < ApplicationController
   def home
   end
+
+  def dashboard
+    @current_user_bookings = current_user.bookings
+    @current_user_spaceships = current_user.spaceships
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: "pages#home"
+  get 'pages/dashboard', to: 'pages#dashboard'
+  # resources :pages, only: %i[dashboard home]
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
