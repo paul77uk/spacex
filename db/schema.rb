aab0031465cfe50f35fa8661426d98a0a6cca799
Cedric Garcia <cgdm@tutanota.com>
2023-08-29 11:56:26 +0200

    corrected attribute

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_08_28_155306) do
+ActiveRecord::Schema[7.0].define(version: 2023_08_29_095419) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -31,7 +31,6 @@ ActiveRecord::Schema[7.0].define(version: 2023_08_28_155306) do
     t.string "color"
     t.integer "seats"
     t.integer "year"
-    t.string "image_url"
     t.text "description"
     t.boolean "available"
     t.bigint "user_id", null: false
