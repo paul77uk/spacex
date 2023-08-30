cc5002f5f6d9ddc3a73d688e7fbd9af005708db4
Sophie Watson <semwatson09@gmail.com>
2023-08-30 16:02:27 +0200

    spaceship edit and update functions added

--- db/schema.rb
+++ db/schema.rb
@@ -64,8 +64,8 @@ ActiveRecord::Schema[7.0].define(version: 2023_08_30_121114) do
     t.bigint "user_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.string "address"
     t.integer "price"
+    t.string "address"
     t.index ["user_id"], name: "index_spaceships_on_user_id"
   end
 
