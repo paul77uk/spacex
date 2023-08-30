db4c4aed2950a2cb60d5acbfdf3cd8c9824cc522
Sophie Watson <semwatson09@gmail.com>
2023-08-30 13:05:24 +0200

    fix hopefully

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_08_30_104526) do
+ActiveRecord::Schema[7.0].define(version: 2023_08_30_110358) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -64,7 +64,6 @@ ActiveRecord::Schema[7.0].define(version: 2023_08_30_104526) do
     t.bigint "user_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.string "address"
     t.index ["user_id"], name: "index_spaceships_on_user_id"
   end
 
