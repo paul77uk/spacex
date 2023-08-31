33a59c552ac5fcef6d9c3cd820d9e51a18d3c8ec
Cedric Garcia <cgdm@tutanota.com>
2023-08-31 18:19:19 +0200

    Save

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_08_31_102521) do
+ActiveRecord::Schema[7.0].define(version: 2023_08_31_133128) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -73,8 +73,8 @@ ActiveRecord::Schema[7.0].define(version: 2023_08_31_102521) do
     t.bigint "user_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.string "address"
     t.integer "price"
+    t.string "address"
     t.float "latitude"
     t.float "longitude"
     t.index ["user_id"], name: "index_spaceships_on_user_id"
